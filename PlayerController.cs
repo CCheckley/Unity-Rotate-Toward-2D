c21cee0e2cf281a0ed01d9b96f5ab96f608bf1c2
unknown <connor.checkley@gmail.com>
2020-03-29 12:31:45 +0100

    Renamed functions, set rigidbody for frictionless topdown movement

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,27 +1,38 @@
 ﻿using UnityEngine;
 
+// Ensure the component is present on the gameobject the script is attached to
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    // Local rigidbody variable to hold a reference to the attached Rigidbody2D component
     new Rigidbody2D rigidbody2D;
-    
+
     void Awake()
     {
+        // Setup Rigidbody for frictionless top down movement and dynamic collision
         rigidbody2D = GetComponent<Rigidbody2D>();
-    }
-    void Update()
-    {
-        Vector3 targetDelta = GetTargetDelta();
-        Rotate(targetDelta);
+
+        rigidbody2D.isKinematic = false;
+        rigidbody2D.angularDrag = 0.0f;
+        rigidbody2D.gravityScale = 0.0f;
     }
 
-    Vector3 GetTargetDelta()
+    void Update()
     {
+        // Get direction to mouse position
+        Vector3 targetDirection = GetTargetDirection();
+        Rotate(targetDirection);
+    }
+
+    Vector3 GetTargetDirection()
+    {
+        // Get the difference between current location and mouse world position
         return Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
     }
 
-    void Rotate(Vector2 rotationDelta)
+    void Rotate(Vector2 targetDirection)
     {
-        rigidbody2D.rotation = Mathf.Atan2(rotationDelta.y, rotationDelta.x) * Mathf.Rad2Deg;
+        // Set rigidbody rotation based on direction to target
+        rigidbody2D.rotation = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;// Convert radians to angle in degrees
     }
 }
